33102e4d30f47e3ec2de445454a899121fa6dc72
espang <eike.spang@gmail.com>
2019-01-11 21:02:54 +0000

    update projects and readme

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # roaring
 
+[![Clojars Project](https://img.shields.io/clojars/v/roaring.svg)](https://clojars.org/roaring)
+
 A Clojure library designed to use java roaring bitmaps.
 
 ## Usage
--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject roaring "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "wrapper around https://github.com/RoaringBitmap/RoaringBitmap"
+  :url "https://github.com/espang/roaring"
   :license {:name "Apache 2.0"
             :url "https://www.apache.org/licenses/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
